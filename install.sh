97dab91f7f36b3619f9be1b821426cd5ed3bd9a5
David Medina <davidmedina9@gmail.com>
2011-08-18 00:50:00 +0200

    Install bash_profile

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
+ln -s $HOME/.dotfiles/bashrc $HOME/.bash_profile
 ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/vim $HOME/.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
