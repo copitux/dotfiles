b316acaacb3c197d5a951ffe81573c1cf00f36e9
David Medina <davidmedina9@gmail.com>
2012-10-17 12:31:07 +0200

    Pyflakes in the name of speed

--- reminders/linux.sh
+++ reminders/linux.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install build-essential git vim-gtk curl exuberant-ctags ack-grep python-setuptools python-dev
+sudo apt-get install build-essential git vim-gtk curl exuberant-ctags ack-grep python-setuptools python-dev pep8 pyflakes
