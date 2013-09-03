bd13ebcd5e0111e06cd30e6242125665e28feba9
David Medina <davidmedina9@gmail.com>
2013-09-03 22:22:53 +0200

    Pre vim

--- prompt.bash
+++ prompt.bash
@@ -5,29 +5,16 @@ GIT_PS1_SHOWCOLORHINTS=1
 GIT_PS1_DESCRIBE_STYLE="branch"
 GIT_PS1_SHOWUPSTREAM="auto git"
 
-Yellow="\[\033[0;33m\]"
-Magenta="\[\033[1;95m\]" 
-BoldGreen="\[\033[1;32m\]"    # Green
-BoldBlue="\[\033[1;34m\]"     # Blue
+YELLOW="\[\033[0;33m\]"
+BLUE="\[\033[0;34m\]"
+COLOR_NONE="\[\033[0m\]"
 
-# High Intensty
-IntenseBlack="\[\033[0;90m\]" # Grey
-
-# Bold High Intensty
-Magenta="\[\033[1;95m\]"      # Purple
-
-# Regular Colors
-Yellow="\[\033[0;33m\]"
-White='\[\033[37m\]'
-Red="\[\033[0;31m\]"
-Blue="\[\033[0;34m\]"
-
-Color_none="\[\033[0m\]"
-Branch_icon='⎇ '
+BRANCH_ICON='⎇ '
 VIRTUALENV_ICON='⚡'
+
 virtualenv() {
   if [[ -n ${VIRTUAL_ENV} ]]; then
-    echo "[${VIRTUALENV_ICON} ${Yellow}$(basename $VIRTUAL_ENV)${Color_none}] "
+    echo "[${VIRTUALENV_ICON} ${YELLOW}$(basename $VIRTUAL_ENV)${COLOR_NONE}] "
   fi
 }
-PROMPT_COMMAND='__git_ps1 "$(virtualenv)" "${Blue}\w${Color_none} \$ " "[${Branch_icon} %s] "'
+PROMPT_COMMAND='__git_ps1 "$(virtualenv)" "${BLUE}\w${COLOR_NONE} \$ " "[${BRANCH_ICON} %s] "'
